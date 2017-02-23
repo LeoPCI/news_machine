52c68198b7d28908be46aabdb313aa46e17cc9cd
Leo <leoproechel@gmail.com>
2017-02-23 10:27:31 -0500

    added deaths

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+//define date
 	today = new Date()
 	year = today.getFullYear();
 	day = today.getDate();
@@ -15,17 +16,23 @@ $(document).ready(function() {
 		last_month_year = year-1
 	};
 
+//add date
 	$("h1").append(month + " " + day + ", " + year)
 
-
+//define source urls
 	ongoing_protests="https://en.wikipedia.org/w/api.php?action=parse&page=List_of_ongoing_protests&contentmodel=wikitext&prop=wikitext&format=json"
 	ongoing_armed_conflicts="https://en.wikipedia.org/w/api.php?action=parse&page=List_of_ongoing_armed_conflicts&contentmodel=wikitext&prop=wikitext&format=json"
 	terrorist_attacks="https://en.wikipedia.org/w/api.php?action=parse&page=List_of_terrorist_incidents_in_"+month+"_"+year+"&contentmodel=wikitext&prop=wikitext&format=json"
 	terrorist_attacks_2="https://en.wikipedia.org/w/api.php?action=parse&page=List_of_terrorist_incidents_in_"+last_month+"_"+last_month_year+"&contentmodel=wikitext&prop=wikitext&format=json"
 	heads_of_state="https://en.wikipedia.org/w/api.php?action=parse&page=List_of_current_state_leaders_by_date_of_assumption_of_office&contentmodel=wikitext&prop=wikitext&section=1&format=json"
+	notable_deaths="https://en.wikipedia.org/w/api.php?action=parse&page=Deaths_in_"+year+"&contentmodel=wikitext&prop=wikitext&section=1&format=json"
 
 //get ongoing protests
-	$.get( ongoing_protests, function( data ) {
+$.ajax({
+    url: ongoing_protests,
+    jsonp: "callback",
+    dataType: "jsonp",
+    success: function( data ) {
 	  var listvar = data["parse"]["wikitext"]["*"]
 	  listvar = listvar.split('==List of ongoing civil unrests and protests==');
 	  listvar = listvar[1].split('align=center');
@@ -45,19 +52,23 @@ $(document).ready(function() {
 	  	  	if (isNaN(parseInt(died))) {died=thing[4].split('{{')[0]};
 		  var url = "https://en.wikipedia.org/wiki/" + name.replace(/ /g, "_")
 
-	      $("#ongoing_protests").append()
-
 
 		  $("#ongoing_protests").append("<a target='_blank' href=" + url + ">" + title + "</a><br>SINCE: " + date + " <br>DEAD: " + died + "<br><br>" );
 	  	};
 	  };
 	  getinfo(listvar)
 	  getinfo(listvar2)
-	});
+    }
+});
 
 
 //get ongoing armed conflicts
-	$.get( ongoing_armed_conflicts, function( data ) {
+$.ajax({
+    url: ongoing_armed_conflicts,
+    jsonp: "callback",
+    dataType: "jsonp",
+    success: function( data ) {
+
 	  var listvar = data["parse"]["wikitext"]["*"]
 	  listvar = listvar.split('==10,000+ deaths in current or past year==');
 	  listvar = listvar[1].split('align=center');
@@ -89,45 +100,16 @@ $(document).ready(function() {
 	  	};
 	  };
 	  getinfo(listvar)
-	});
-
+    }
+});
 
 //add last month's attacks if within past week
 if (day < 7) {
-	$.get( terrorist_attacks_2, function( data ) {
-
-		attacks = data["parse"]["wikitext"]["*"].split('== '+window.last_month+' ==')[1].split("|-")
-
-		for (var i = 2; i < attacks.length; i++) {
-			
-			var context = attacks[i].split("[[")
-			var perpetrator = context[context.length-2].split("]]")[0].split("|")
-			    perpetrator = perpetrator[perpetrator.length-1]
-			    if (perpetrator=="01") {perpetrator = "unknown"};
-			var conflict = context[context.length-1].split("|")
-			    conflict = conflict[conflict.length-1].slice(0,-3)
-
-			var attack = attacks[i].split("|")
-			var date = attack[1]
-			var type = attack[2]
-			var dead = attack[3]
-			var where = attack[6].split("}}")[0]
-			var description = attack[7].split('{{')[0].replace(/\[/g, "").replace(/\]/g, "")
-
-			var now = new Date().toLocaleString().split("/")[1]
-			var month = new Date().getMonth();
-
-			if (date > 29-now) {
-		    	$("#terrorist_attacks").prepend("WHEN: " + window.last_month + " " + date + "<br>WHAT: " + type + "<br> WHERE: " + where + "<br>DEAD: " + dead + "<br>PERPETRATOR: " + perpetrator + "<br><br>" );
-		    };
-		};
-
-
-	});
-};
-
-//get recent terrorist attacks
-	$.get( terrorist_attacks, function( data ) {
+$.ajax({
+    url: terrorist_attacks,
+    jsonp: "callback",
+    dataType: "jsonp",
+    success: function( data ) {
 
 		attacks = data["parse"]["wikitext"]["*"].split('== '+window.month+' ==')[1].split("|-")
 
@@ -137,10 +119,9 @@ if (day < 7) {
 			if (source!=null) { 
 				source = source.split("|")[0]
 				source = source.replace("</ref", "").replace("<ref", "").replace(" ", "").replace(">", "").replace(/\{\{citeweb/, "").replace(/\>/, "")
+				if (source.slice(-1)=="/") {source = source.slice(0, -1)};
 				source = 'http' + source
-				//source = '<a target="_blank" href=' + source + '>story</a><br>'
 			};
-							//alert(source)
 
 
 			var context = attacks[i].split("[[")
@@ -153,9 +134,56 @@ if (day < 7) {
 			var attack = attacks[i].split("|")
 			var date = attack[1]
 			var type = attack[2]
-			var dead = attack[3]
-			var where = attack[6].split("}}")[0]
-			var description = attack[7].split('{{')[0].replace(/\[/g, "").replace(/\]/g, "")
+			var dead = attack[4]
+			var where = attacks[i].split("{{")[1].split("}}")[0].split("|")[1]
+
+			var description = attack[9].split('{{')[0].replace(/\[/g, "").replace(/\]/g, "").split("http")[0]
+
+			var now = new Date().toLocaleString().split("/")[1]
+			var month = new Date().getMonth();
+
+			if (now-date < 8) {
+		    	$("#terrorist_attacks").prepend("<a target='_blank' href=" + source + ">" + type + "</a><br>WHEN: " + window.last_month + " " + date + "<br> WHERE: " + where + "<br>DEAD: " + dead + "<br>PERPETRATOR: " + perpetrator + "<br><br>" );
+		    };
+		};
+    }
+});
+};
+
+//get recent terrorist attacks
+$.ajax({
+    url: terrorist_attacks,
+    jsonp: "callback",
+    dataType: "jsonp",
+    success: function( data ) {
+
+		attacks = data["parse"]["wikitext"]["*"].split('== '+window.month+' ==')[1].split("|-")
+
+		for (var i = 2; i < attacks.length; i++) {
+			
+			var source=attacks[i].split("http")[1]
+			if (source!=null) { 
+				source = source.split("|")[0]
+				source = source.replace("</ref", "").replace("<ref", "").replace(" ", "").replace(">", "").replace(/\{\{citeweb/, "").replace(/\>/, "")
+				if (source.slice(-1)=="/") {source = source.slice(0, -1)};
+				source = 'http' + source
+			};
+
+
+			var context = attacks[i].split("[[")
+			var perpetrator = context[context.length-2].split("]]")[0].split("|")
+			    perpetrator = perpetrator[perpetrator.length-1]
+			    if (perpetrator=="01") {perpetrator = "unknown"};
+			var conflict = context[context.length-1].split("|")
+			    conflict = conflict[conflict.length-1].slice(0,-3)
+
+			var attack = attacks[i].split("|")
+			var date = attack[1]
+			var type = attack[2]
+			var dead = attack[4]
+			var where = attacks[i].split("{{")[1].split("}}")[0].split("|")[1]
+
+			var description = attack[9].split('{{')[0].replace(/\[/g, "").replace(/\]/g, "").split("http")[0]
 
 			var now = new Date().toLocaleString().split("/")[1]
 			var month = new Date().getMonth();
@@ -164,11 +192,16 @@ if (day < 7) {
 		    	$("#terrorist_attacks").prepend("<a target='_blank' href=" + source + ">" + type + "</a><br>WHEN: " + window.month + " " + date + "<br> WHERE: " + where + "<br>DEAD: " + dead + "<br>PERPETRATOR: " + perpetrator + "<br><br>" );
 		    };
 		};
+    }
+});
 
 
-	});
-
-	$.get( heads_of_state, function( data ) {
+//get recent heads of state
+$.ajax({
+    url: heads_of_state,
+    jsonp: "callback",
+    dataType: "jsonp",
+    success: function( data ) {
 
 		var heads = data["parse"]["wikitext"]["*"].split("=== "+window.year+" ===")
 		heads = heads[heads.length-1].split('|-')
@@ -187,10 +220,56 @@ if (day < 7) {
 		    	$("#new_heads").prepend( "<a target=_blank href=" + url + ">" + who + "</a><br>" + what + "<br>SINCE: " + month + " " + date + "<br><br>" );
 		    };
 		};
+    }
+});
 
+//get notable deaths
+$.ajax({
+    url: notable_deaths,
+    jsonp: "callback",
+    dataType: "jsonp",
+    success: function( data ) {
 
-	});
+		var days = data["parse"]["wikitext"]["*"].split("===\n")
+		var today = days[1].split("*")
+		if (days.length>1) {var yesterday = days[2].split("*")};		
+		if (days.length>2) {var day3 = days[3].split("*")};		
+		
+		// if (days.length>4) {var day4 = days[4].split("*")};		
+		// if (days.length>5) {var day5 = days[5].split("*")};		
+		// if (days.length>6) {var day6 = days[6].split("*")};		
+		// if (days.length>7) {var day7 = days[7].split("*")};		
 
+		var list_deaths = function(day, location) {
+			for (var i = 1; i < day.length; i++) {
+				var notability = day[i].split(".")[0].split("]]")[1].slice(6)
 
+				var death = day[i].split(">[")[1].split("]</ref>")[0]
+				var person = day[i].split("[[")[1].split("]]")[0].split("|")[0]
+				var url = death.split(" ")[0]
+					if (url.slice(-1)=="/") {url = url.slice(0, -1)};
+				var explanation = death.replace(url, "").replace("/ ", "").split("]")[0]
+
+				$(location).append("<a target=_blank href=" + url + ">" + person + "</a>" + "<br>" + explanation + "<br><br>")
+
+			};
+		};
+
+		$("#notable_deaths").append("<strong>TODAY</strong><br>")
+		list_deaths(today, "#notable_deaths")
+		$("#notable_deaths").append("<strong>YESTERDAY</strong><br>")
+		list_deaths(yesterday, "#notable_deaths")
+		// $("#notable_deaths").append("____<br>")
+		// list_deaths(day3, "#notable_deaths")
+		// $("#notable_deaths").append("____<br>")
+		// list_deaths(day4, "#notable_deaths")
+		// $("#notable_deaths").append("____<br>")
+		// list_deaths(day5, "#notable_deaths")
+		// $("#notable_deaths").append("____<br>")
+		// list_deaths(day6, "#notable_deaths")
+		// $("#notable_deaths").append("____<br>")
+		// list_deaths(day7, "#notable_deaths")
+    }
+});
 
 });
